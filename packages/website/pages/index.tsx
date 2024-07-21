6d6b7ab66aa178114a0545cbd9821f9fb6315c85
Matthijs van Boven <matthijsvanboven@toeleveringonline.nl>
2024-07-21 16:32:45 +0200

    feat: added base for mobile filter modal

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,16 +1,25 @@
 import { useIntl } from 'react-intl'
 import { BoxPage } from '../components/BoxPage/BoxPage.tsx'
-import { FilterProps, Filters, JobCard } from '@publiekepartner/components'
+import { FilterProps, Filters, JobCard, Modal } from '@publiekepartner/components'
 import { Locations } from '../mockdata/Locations.ts'
 import { Categories } from '../mockdata/Categories.ts'
-import { Grid } from '@mui/material'
+import { Box, Grid, IconButton, useMediaQuery, useTheme } from '@mui/material'
 import { Jobs } from '../mockdata/Jobs.ts'
 import { useState } from 'react'
+import { useStateToggle } from '@publiekepartner/components/src/utils/useStateToggle.ts'
+import { Tune } from '@mui/icons-material'
 
 const Index = () => {
   const { formatMessage } = useIntl()
   const [FilteredLocations, setFilteredLocations] = useState<Array<FilterProps>>([])
   const [FilteredCategories, setFilteredCategories] = useState<Array<FilterProps>>([])
+  const theme = useTheme()
+  const isMobileDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const {
+    isOpen: filtersModalIsOpen,
+    handleClose: filtersModalHandleClose,
+    handleOpen: filtersModalHandleOpen,
+  } = useStateToggle()
 
   const filterCategories = async (category: FilterProps) => {
     const isFiltered = FilteredCategories.includes(category)
@@ -30,6 +39,21 @@ const Index = () => {
     }
   }
 
+  const filters = (
+    <Box>
+      <Filters
+        title={formatMessage({ id: 'filters.categories.title' })}
+        handleFilter={filterCategories}
+        filters={Categories}
+      />
+      <Filters
+        title={formatMessage({ id: 'filters.locations.title' })}
+        handleFilter={filterLocations}
+        filters={Locations}
+      />
+    </Box>
+  )
+
   return (
     <BoxPage
       seo={{
@@ -39,18 +63,31 @@ const Index = () => {
       title={formatMessage({ id: 'homepage.title' })}
     >
       <Grid container spacing={2}>
-        <Grid item xs={6} sm={4} md={4} lg={4}>
-          <Filters
-            title={formatMessage({ id: 'filters.categories.title' })}
-            handleFilter={filterCategories}
-            filters={Categories}
-          />
-          <Filters
-            title={formatMessage({ id: 'filters.locations.title' })}
-            handleFilter={filterLocations}
-            filters={Locations}
-          />
-        </Grid>
+        {isMobileDown ? (
+          <Grid item sm={12}>
+            <IconButton
+              size="large"
+              aria-label={formatMessage({ id: 'filters.modal.title' })}
+              aria-controls="modal-filters"
+              aria-haspopup="true"
+              onClick={filtersModalHandleOpen}
+              color="inherit"
+            >
+              <Tune color="primary" />
+            </IconButton>
+            <Modal
+              open={filtersModalIsOpen}
+              title={formatMessage({ id: 'filters.modal.title' })}
+              handleClose={filtersModalHandleClose}
+            >
+              {filters}
+            </Modal>
+          </Grid>
+        ) : (
+          <Grid item xs={6} sm={4} md={4} lg={4}>
+            {filters}
+          </Grid>
+        )}
         {Jobs.map((Job) => (
           <Grid item key={Job.title} xs={12} sm={8} md={8} lg={8}>
             <JobCard {...Job} />
